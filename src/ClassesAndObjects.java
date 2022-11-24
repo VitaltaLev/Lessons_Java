b557de431046d936562d794eb1aafc55553f8e06
Vitalya <vitalya.lev@mail.ru>
2022-11-24 21:50:54 +0300

    Инкапсуляция пример (без применения инкапсуляции)

---
+++ src/ClassesAndObjects.java
@@ -0,0 +1,23 @@
+/**
+ * Инкапсуляция Сеттеры и геттеры
+ */
+public class ClassesAndObjects {
+    public static void main(String[] arsg) {
+        Person person1 = new Person();
+        person1.name = "КакоетоИмя"; // мы обращаемся к полям  строка кода 13, 14 через точку (прямое обращение)
+        person1.age = 12;
+    }
+}
+
+class Person{
+    String name;                    // при смене имени данного поля придется переименовывать
+    int age;
+
+    int calculateYearsToRetirement(){
+        int years = 65-age;
+        return years;
+    }
+    void speak(){
+        for(int i = 0; i<3; i++);
+    }
+}
